ada205aa285de2e99c4c9d8c85bf373b40301664
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-05 14:08:08 -0600

    created new objective in 2d arrays and added object arrays exercise to go over

---
+++ arraysAndLoops/objectArrays.js
@@ -0,0 +1,13 @@
+let People = [
+    {"name": 'Jacob', "age": 25, "height": "5'11", "Gender": "M"},
+    {"name": 'Nik', "age": 73, "height": "4'9", "Gender": "M"},
+    {"name": 'Lilly', "age": 34, "height": "5'0", "Gender": "F"},
+    {"name": 'Betsy', "age": 92, "height": "4'3", "Gender": "F"},
+    {"name": 'Gerald', "age": 97, "height": "6'7", "Gender": "M"},
+    {"name": 'Brenda', "age": 19, "height": "5'10", "Gender": "F"},
+];
+
+// for loop through the array displaying the text such as: "Jacob is a Male, he 25 years old, and is 5'11 in height"
+
+
+// forEach loop through the array displaying the text such as: "Jacob is a Male, he 25 years old, and is 5'11 in height"
---
+++ arraysAndLoops/twoDimensionalArrays.js
@@ -0,0 +1,37 @@
+// Person: name, age, height, Gender(M/F) 
+
+let People = [
+    ['Jacob', 25, "5'11", "M"],
+    ['Nik', 73, "4'9", "M"],
+    ['Lilly', 34, "5'0", "F"],
+    ['Betsy', 92, "4'3", "F"],
+    ['Gerald', 97, "6'7", "M"],
+    ['Brenda', 19, "5'10", "F"],
+];
+
+// for loop through the array displaying:
+// Jacob
+// 25
+// 5'11
+// M
+// ~~~~~~~~~~~~~~
+// Nik
+// 73
+// 4'9
+// M
+// ~~~~~~~~~~~~~~
+
+
+
+
+// forEach loop through the array displaying:
+// Jacob
+// 25
+// 5'11
+// M
+// ~~~~~~~~~~~~~~
+// Nik
+// 73
+// 4'9
+// M
+// ~~~~~~~~~~~~~~
