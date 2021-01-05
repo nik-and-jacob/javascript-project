46d88733302f0b6ece8ea3a1a9e2f5708c64d5b1
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-05 13:34:11 -0600

    created template to work with on simple arrays and loops

---
+++ arraysAndLoops/loops.js
@@ -0,0 +1,34 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let animals = ['dog', 'bird', 'lion', 'deer', 'gorilla', 'leopard', 'guinea pig'];
+
+// ForEach
+
+//filter
+
+//map
+
+//sort
+
+//reduce
+
+//for..of
+
+//for..in
+
+//find
+
+//findIndex
+
+//every
+
+//some
+
+//entries/ fromEntries
+
+//includes
+
+//splice
+
+//slice
+
+//concat
