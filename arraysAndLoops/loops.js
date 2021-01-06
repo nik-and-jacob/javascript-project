c234ca2e34970cbd90cf84a6fce73e68db16ca11
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-06 17:13:26 -0600

    completed forEach examples with arrow function

--- arraysAndLoops/loops.js
+++ arraysAndLoops/loops.js
@@ -2,9 +2,13 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let animals = ['dog', 'bird', 'lion', 'deer', 'gorilla', 'leopard', 'guinea pig'];
 
 // ForEach
-numbers.forEach(number => function {
-    console.log(number)
+numbers.forEach((number) => {
+    console.log(number);
 });
+
+animals.forEach((animal) => {
+    console.log(animal);
+})
 //filter
 
 //map
