813b3ca82dfd4b8f20c2c92f6d567e3c9a636fae
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-06 19:34:45 -0600

    Add: .filter and .map

--- arraysAndLoops/cardio-one.js
+++ arraysAndLoops/cardio-one.js
@@ -28,10 +28,12 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-
+const oldAf = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.log(oldAf);
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
-
+const fullNames = inventors.map( inventor => `${inventor.first} ${inventor.last}`)
+console.table(fullNames);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
