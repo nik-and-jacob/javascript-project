fc86829c8078b3321548bb5040511c74b7603905
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-07 21:46:36 -0600

    added examples and description for the use of the .find() method

--- arraysAndLoops/loops.js
+++ arraysAndLoops/loops.js
@@ -68,7 +68,14 @@ for (const property in object) {
 };
 
 //find
+// The find() method returns the value of the first element in the provided
+// array that satisfies the provided testing function. If no values satisfies
+// the testing function, undefined is returned.
+const firstWithFourLetters = animals.find(animal => animal.length <= 4);
+console.log(firstWithFourLetters);
 
+const firstWithMoreThanFourLetters = animals.find(animal => animal.length > 4);
+console.log(firstWithMoreThanFourLetters);
 //findIndex
 
 //every
