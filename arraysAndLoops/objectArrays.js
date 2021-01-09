4aef5ab5b686b515819e75cc0a1ac6493491b39e
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-09 11:51:25 -0600

    feat: this adds styling for DOM rendering

--- arraysAndLoops/objectArrays.js
+++ arraysAndLoops/objectArrays.js
@@ -1,13 +1,29 @@
-let People = [
-    {"name": 'Jacob', "age": 25, "height": "5'11", "Gender": "M"},
-    {"name": 'Nik', "age": 73, "height": "4'9", "Gender": "M"},
-    {"name": 'Lilly', "age": 34, "height": "5'0", "Gender": "F"},
-    {"name": 'Betsy', "age": 92, "height": "4'3", "Gender": "F"},
-    {"name": 'Gerald', "age": 97, "height": "6'7", "Gender": "M"},
-    {"name": 'Brenda', "age": 19, "height": "5'10", "Gender": "F"},
+let people = [
+    {"name": 'Jacob', "age": 25, "height": "5'11", "gender": "M"},
+    {"name": 'Nik', "age": 73, "height": "4'9", "gender": "M"},
+    {"name": 'Lilly', "age": 34, "height": "5'0", "gender": "F"},
+    {"name": 'Betsy', "age": 92, "height": "4'3", "gender": "F"},
+    {"name": 'Gerald', "age": 97, "height": "6'7", "gender": "M"},
+    {"name": 'Brenda', "age": 19, "height": "5'10", "gender": "F"},
 ];
 
 // for loop through the array displaying the text such as: "Jacob is a Male, he 25 years old, and is 5'11 in height"
 
 
 // forEach loop through the array displaying the text such as: "Jacob is a Male, he 25 years old, and is 5'11 in height"
+
+function peopleCards() {
+    const peeps = people;
+    const html = `<div class="peopleCard">
+    <h2>${peeps.name} — ${peeps.age}</h2>
+    <p>They are ${peeps.height}" tall and ${peeps.gender === 'M' ? 'ale' : 'Female'}</p>
+    <button class="delete"><i class="bi bi-emoji-dizzy"></i Delete</button>
+     </div>`;
+}
+console.log(peopleCards({people}));
+
+const div = document.createElement('div');
+document.body.append(div);
+div.classList.add('wrapper')
+const string = `<h1>Please, let this work</h1><p> -thanks.</p>`;
+div.insertAdjacentHTML("afterbegin", string);
